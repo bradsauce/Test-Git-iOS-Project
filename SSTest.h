0fcc5e064771b573035ba54b0521b7c751a94785
Brad Thele <bradthele@Brads-MacBook-Pro.local>
2014-06-30 13:38:36 -0400

    added a test class

---
+++ SSTest.h
@@ -0,0 +1,13 @@
+//
+//  SSTest.h
+//  Git Test
+//
+//  Created by Brad Thele on 6/30/14.
+//  Copyright (c) 2014 Scoops. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSTest : NSObject
+
+@end
---
+++ SSTest.m
@@ -0,0 +1,13 @@
+//
+//  SSTest.m
+//  Git Test
+//
+//  Created by Brad Thele on 6/30/14.
+//  Copyright (c) 2014 Scoops. All rights reserved.
+//
+
+#import "SSTest.h"
+
+@implementation SSTest
+
+@end
